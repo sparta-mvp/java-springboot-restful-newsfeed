75739ef4fe6e6ea0860667ffce56a351ad780de0
euuns <dmsu2@naver.com>
2025-02-15 23:00:01 +0900

    Feat(Comment): Create ReqeustDto & ResponseDto

---
+++ src/main/java/com/example/newsfeed/comment/dto/CommentResponseDto.java
@@ -0,0 +1,30 @@
+package com.example.newsfeed.comment.dto;
+
+import com.example.newsfeed.comment.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class CommentResponseDto {
+
+    private final String writer;
+    private final String postTitle;
+    private final String contents;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private final LocalDateTime updateAt;
+
+
+    public static CommentResponseDto toDto(Comment entity){
+        return CommentResponseDto.builder()
+                .writer(entity.getUser().getName())
+                .postTitle(entity.getPost().getTitle())
+                .contents(entity.getContents())
+                .updateAt(entity.getUpdatedAt())
+                .build();
+    }
+}
