43441d8871837448d57b5f39dbf94ae154bb351e
jun5-soodal <j01024749503@gmail.com>
2025-02-16 22:57:17 +0900

    feat(user):User 엔티티에 BaseEntity 상속 적용
    - User 엔티티가 생성일(createdAt), 수정일(updatedAt)을 자동 관리할 수 있도록 BaseEntity 상속

--- src/main/java/com/example/newsfeed/user/entity/User.java
+++ src/main/java/com/example/newsfeed/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.newsfeed.user.entity;
 
+import com.example.newsfeed.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,7 +17,7 @@ import lombok.Getter;
 @Getter
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
