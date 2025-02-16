7760da67a80d3cb5b720f3304f59bbf32b932e23
jun5-soodal <j01024749503@gmail.com>
2025-02-16 13:33:26 +0900

    feat(auth):로그인 기능 추가
    - 로그인 API ('POST /api/v1/auth/login') 구현
    - (common/exception) 'NewsfeedAppException'에 기존 예외를 cause로 받을 수 있도록 생성자 추가

---
+++ src/main/java/com/example/newsfeed/auth/service/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.example.newsfeed.auth.service;
+
+import com.example.newsfeed.auth.dto.LoginUser;
+import com.example.newsfeed.auth.exception.LoginFailedException;
+import com.example.newsfeed.common.config.PasswordEncoder;
+import com.example.newsfeed.user.entity.User;
+import com.example.newsfeed.user.exception.UserNotFoundException;
+import com.example.newsfeed.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public LoginUser login(String email, String password) {
+        User user = getUserOrThrow(email);
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new LoginFailedException();
+        }
+
+        return LoginUser.of(user.getEmail(), user.getName());
+
+    }
+
+    private User getUserOrThrow(String email) {
+        try {
+            return userService.getUserByEmail(email);
+        } catch (UserNotFoundException e) {
+            throw new LoginFailedException(e);
+        }
+    }
+}
