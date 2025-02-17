03ef7cc826f6844c73788c348ffa6b422fd66043
euuns <dmsu2@naver.com>
2025-02-17 17:31:18 +0900

    Revert "Fix(ErrorCode): COMMENT_NOT_FOUND 메세지 오타 변경"
    This reverts commit 5e536a37f5b5f2502ef09e342ca5cd26da97ca3f.

--- src/main/java/com/example/newsfeed/common/exception/ErrorCode.java
+++ src/main/java/com/example/newsfeed/common/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"INVALID_PASSWORD" , "비밀번호가 일치하지 않습니다."),
     SCHEDULE_NOT_FOUND(HttpStatus.BAD_REQUEST,"SCHEDULE_NOT_FOUND","일정을 찾을 수 없습니다." ),
     UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN,"UNAUTHORIZED_ACCESS","해당 작업을 수행할 권한이 없습니다"),
-    COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST,"COMMENT_NOT_FOUND", "댓글을 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST,"COMMENT_NOT_FOUND", "일정을 찾을 수 없습니다."),
     DUPLICATE_MEMBER(HttpStatus.BAD_REQUEST,"DUPLICATE_MEMBER","이미 존재하는 회원입니다." ),
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED,"LOGIN_FAILED","아이디 또는 비밀번호가 맞지 않습니다."),
     PAGE_NOT_POSITIVE(HttpStatus.NOT_FOUND,"PAGE_NOT_POSITIVE", "0이하의 페이지 번호 요청"),
