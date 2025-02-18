57942295623ace4e2d82b12dbfb3a70ef359de1b
euuns <dmsu2@naver.com>
2025-02-18 19:50:56 +0900

    Fix(comment): Add entity Lazy loading

--- src/main/java/com/example/newsfeed/comment/entity/Comment.java
+++ src/main/java/com/example/newsfeed/comment/entity/Comment.java
@@ -4,6 +4,7 @@ import com.example.newsfeed.post.Post;
 import com.example.newsfeed.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,11 +25,11 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(referencedColumnName = "id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(referencedColumnName = "id")
     private Post post;
 
