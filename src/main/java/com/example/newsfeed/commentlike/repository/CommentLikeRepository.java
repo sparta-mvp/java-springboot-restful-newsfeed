cbc054e8db4d82733f62b280c163bc6503e889cb
jun5-soodal <j01024749503@gmail.com>
2025-02-19 12:45:17 +0900

    refactor(commentLike): EntityManager 대신 JPA Repository 사용하도록 변경
    - 기존 EntityManager 직접 사용 코드 제거

--- src/main/java/com/example/newsfeed/commentlike/repository/CommentLikeRepository.java
+++ src/main/java/com/example/newsfeed/commentlike/repository/CommentLikeRepository.java
@@ -1,49 +1,23 @@
 package com.example.newsfeed.commentlike.repository;
 
 import com.example.newsfeed.commentlike.entity.CommentLike;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
 @Repository
-@RequiredArgsConstructor
-public class CommentLikeRepository {
+public interface CommentLikeRepository extends JpaRepository<CommentLike,Long> {
 
-    private final EntityManager em;
 
-    public boolean existsByCommentIdAndUserId(Long commentId, Long userId) {
-        return em.createQuery("select cl from CommentLike cl where cl.comment.id = :commentId and cl.user.id = :userId")
-                .setParameter("commentId", commentId)
-                .setParameter("userId", userId)
-                .setMaxResults(1)
-                .getResultStream()
-                .findFirst()
-                .isPresent();
+    @Query("select CASE WHEN count(cl) > 0 then true else false end from CommentLike cl where cl.comment.id = :commentId and cl.user.id = :userId")
+    boolean existsByCommentIdAndUserId(Long commentId, Long userId);
 
-    }
+    @Query("select cl from CommentLike cl where cl.comment.id = :commentId and cl.user.id = :userId")
+     Optional<CommentLike> findByCommentIdAndUserId(Long commentId, Long userId);
 
-    public void save(CommentLike commentLike) {
-        em.persist(commentLike);
-    }
+    Long countByCommentId(Long commentId);
 
-    public long countByCommentId(Long commentId) {
-        return em.createQuery("select count(cl) from CommentLike cl where cl.comment.id = :commentId", Long.class)
-                .setParameter("commentId",commentId)
-                .getSingleResult();
-    }
-
-    public Optional<CommentLike> findByCommentIdAndUserId(Long commentId, Long userId) {
-        return em.createQuery("select cl from CommentLike cl where cl.comment.id = :commentId and cl.user.id = :userId", CommentLike.class)
-                .setParameter("commentId", commentId)
-                .setParameter("userId", userId)
-                .getResultStream()
-                .findFirst();
-    }
-
-    public void delete(CommentLike commentLike) {
-        em.remove(commentLike);
-    }
 }
--- src/main/java/com/example/newsfeed/commentlike/service/component/CommentLikeReader.java
+++ src/main/java/com/example/newsfeed/commentlike/service/component/CommentLikeReader.java
@@ -1,12 +1,10 @@
 package com.example.newsfeed.commentlike.service.component;
 
-import com.example.newsfeed.commentlike.entity.CommentLike;
 import com.example.newsfeed.commentlike.repository.CommentLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +13,6 @@ public class CommentLikeReader {
     private final CommentLikeRepository commentLikeRepository;
     public boolean existsByCommentIdAndUserId(Long commentId, Long userId) {
         return commentLikeRepository.existsByCommentIdAndUserId(commentId, userId);
-    }
-
-    public Optional<CommentLike> findByCommentIdAndUserId(Long commentId, Long userId) {
-        return commentLikeRepository.findByCommentIdAndUserId(commentId, userId);
     }
 
     public Long countByCommentId(Long commentId) {
